82d4001906ff660d1b6c8f73deebb9cee24374e1
Oana Vezentan <oana.vezentan@gmail.com>
2017-09-21 17:14:07 +0200

    Added header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-
+import { TodoHeaderComponent } from './components/todo-header/todo-header.component';
 import { TodoInputComponent } from './components/todo-input/todo-input.component';
 
 import { TodoService } from './services/todo.service';
@@ -12,7 +12,7 @@ import { TodoItemComponent } from './components/todo-item/todo-item.component';
 @NgModule({
   declarations: [
     AppComponent,
-
+    TodoHeaderComponent,
     TodoInputComponent,
     TodoItemComponent
   ],
